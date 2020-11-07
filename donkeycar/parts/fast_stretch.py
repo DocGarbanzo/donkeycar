4992fe072ab62d4c55782d57a91e7221a7316be8
DocGarbanzo <dirk.prange@web.de>
2020-11-07 18:16:50 +0000

    Make fast_stretch run on batches

--- donkeycar/parts/fast_stretch.py
+++ donkeycar/parts/fast_stretch.py
@@ -11,72 +11,66 @@ T = -0.50  # Gamma boost
 Epsilon = 1  # Epsilon
 
 
-def fast_stretch(image, C, Ts, Tr, T, debug=False):
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    (h, s, v) = cv2.split(hsv)
-    input = v
-    shape = input.shape
-    rows = shape[0]
-    cols = shape[1]
-    size = rows * cols
-    if debug:
-        start = time.time()
-    mean = np.mean(input)
-    t = mean / 255
-    Sl = t * (1 - Ts) + C
-    Sh = t * (1 + Ts) + C
+def fast_stretch(images, C, Ts):
+    hues = []
+    saturations = []
+    Xls = []
+    Xhs = []
+    inputs = []
+    outputs = []
+    for img in images:
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        (h, s, v) = cv2.split(hsv)
+        inputs.append(v)
+        hues.append(h)
+        saturations.append(s)
+        shape = v.shape
+        rows = shape[0]
+        cols = shape[1]
+        size = rows * cols
 
-    gamma = 1.
-    if t <= T:
-        gamma = max((1 + (t - T)), Tr)
+        mean = np.mean(v)
+        t = mean / 255
+        Sl = t * (1 - Ts) + C
+        Sh = t * (1 + Ts) + C
 
-    if debug:
-        time_taken = (time.time() - start) * 1000
-        print('Preprocessing time %s' % time_taken)
-        start = time.time()
+        histogram = np.bincount(v.reshape(size, ), minlength=256)
+        # Walk histogram
+        Xl = 0
+        Xh = 255
+        targetFl = Sl * size
+        targetFh = Sh * size
 
-    histogram = cv2.calcHist([input], [0], None, [256], [0, 256])
-    # Walk histogram
-    Xl = 0
-    Xh = 255
-    targetFl = Sl * size
-    targetFh = Sh * size
+        count = 0
+        while count < targetFl and Xl < 256:
+            count += histogram[Xl]
+            Xl += 1
 
-    count = 0
-    while count < targetFl and Xl < 256:
-        count += histogram[Xl]
-        Xl += 1
+        count = 0
+        while count < size - targetFh and Xh > -1:
+            count += histogram[Xh]
+            Xh -= 1
 
-    count = 0
-    while count < size - targetFh and Xh > -1:
-        count += histogram[Xh]
-        Xh -= 1
+        Xls.append(Xl)
+        Xhs.append(Xh)
 
-    if debug:
-        time_taken = (time.time() - start) * 1000
-        print('Histogram Binning %s' % time_taken)
-        start = time.time()
-
+    inputs_arr = np.array(inputs)
+    Xl_arr = np.array(Xls)
+    Xh_arr = np.array(Xhs)
     # Vectorized ops
-    output = np.where(input <= Xl, 0, input)
-    output = np.where(output >= Xh, 255, output)
-    output = np.where(np.logical_and(output > Xl, output < Xh),
-                      # 255 * np.power((output - Xl) / max((Xh - Xl),
-                      # Epsilon), gamma),
-                      255 * (output - Xl) / max((Xh - Xl), Epsilon),
+    output = np.where(inputs_arr <= Xl_arr, 0, inputs_arr)
+    output = np.where(output >= Xh_arr, 255, output)
+    output = np.where(np.logical_and(output > Xl_arr, output < Xh_arr),
+                      255 * (output - Xl_arr) / max((Xh_arr - Xl_arr), Epsilon),
                       output)
-    # max to 255
+    # max to 255 and integer casting
     output = np.where(output > 255., 255., output)
     output = np.asarray(output, dtype='uint8')
-    output = cv2.merge((h, s, output))
-    output = cv2.cvtColor(output, cv2.COLOR_HSV2RGB)
-
-    if debug:
-        time_taken = (time.time() - start) * 1000
-        print('Vector Ops %s' % time_taken)
-        print('t', t, 'Sl', Sl, 'Sh', Sh, 'Xl', Xl, 'Xh', Xh)
-
-    return output
+    for hue, sat, out_single in zip(hues, saturations, output):
+        out_img = cv2.merge((hue, sat, out_single))
+        out_img = cv2.cvtColor(out_img, cv2.COLOR_HSV2RGB)
+        outputs.append(out_img)
+    return outputs
 
 
 if __name__ == "__main__":
@@ -86,7 +80,7 @@ if __name__ == "__main__":
     bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image_data = np.asarray(bgr, dtype=np.uint8)
 
-    stretched = fast_stretch(image_data, debug=True)
+    stretched = fast_stretch(image_data)
     cv2.imshow('Original', image)
     cv2.imshow('Contrast Stretched', stretched)
     cv2.waitKey(0)
