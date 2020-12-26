c697263d45ae8e4852f7c7447660cdc348f30fc3
DocGarbanzo <dirk.prange@web.de>
2020-12-26 12:12:14 +0000

    Added file dialog to read car dir and tub dir

--- donkeycar/management/tub_gui.py
+++ donkeycar/management/tub_gui.py
@@ -1,7 +1,8 @@
 import math
+import os
 from collections import namedtuple
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 import time
 from threading import Thread, active_count, Lock
 from PIL import ImageTk, Image
@@ -80,6 +81,7 @@ class TubUI:
         self.img = self.get_img(self.current_rec)
         self.thread = None
         self.bars = dict()
+        self.car_dir = ''
         self.build_frame()
         self.update()
         self.count = 0
@@ -89,70 +91,98 @@ class TubUI:
         img = Image.fromarray(img_arr)
         return ImageTk.PhotoImage(img)
 
+    def update_tub(self):
+        self.tub = Tub(self.base_path)
+        self.type_dict = dict(zip(self.tub.manifest.inputs,
+                                  self.tub.manifest.types))
+        self.records = [TubRecord(self.config, self.tub.base_path, record)
+                        for record in self.tub]
+        self.len = len(self.records)
+        self.i = 0
+        self.current_rec = self.records[self.i]
+        self.img = self.get_img(self.current_rec)
+
     def build_frame(self):
+        # running row
+        row = 0
+        self.button_car_dir = tk.Button(self.window, text="Car dir",
+                                        command=self.browse_car)
+        self.button_car_dir.grid(row=row, column=0)
+        self.car_dir_label = tk.Label(self.window)
+        self.car_dir_label.grid(row=row, column=1, columnspan=2)
+        self.button_tub_dir = tk.Button(self.window, text="Tub dir",
+                                        command=self.browse_tub)
+        self.button_tub_dir.grid(row=row, column=3)
+        self.tub_dir_label = tk.Label(self.window)
+        self.tub_dir_label.grid(row=row, column=4, columnspan=2)
 
         # Image
+        row += 1
         self.img_frame = tk.Label(self.window, image=self.img, bg='black',
                                   relief=tk.SUNKEN, borderwidth=3)
-        self.img_frame.grid(row=0, column=3, columnspan=4, rowspan=3,
+        self.img_frame.grid(row=row, column=3, columnspan=4, rowspan=3,
                             padx=15, pady=15)
 
         # data box
         self.data_frame = tk.LabelFrame(self.window, padx=10, pady=10)
-        self.data_frame.grid(row=0, column=0, rowspan=3)
+        self.data_frame.grid(row=row, column=0, rowspan=3)
 
         self.var_label = tk.Label(self.data_frame, text='Add/remove')
-        self.var_label.grid(row=0, column=0, sticky=tk.W)
+        self.var_label.grid(row=row, column=0, sticky=tk.W)
         self.var_select = tk.StringVar()
         self.w = ttk.OptionMenu(self.data_frame, self.var_select,
                                 *self.tub.manifest.inputs,
                                 command=self.add_remove_bars)
-        self.w.grid(row=0, column=1, columnspan=2)
-        LabelBar.row += 1
+        self.w.grid(row=row, column=1, columnspan=2)
+        LabelBar.row = row + 1
 
         # control box
         w, h = (3, 1)
         self.ctr_fram = tk.LabelFrame(self.window, padx=10, pady=10)
-        self.ctr_fram.grid(row=0, column=7, rowspan=4)
+        self.ctr_fram.grid(row=row, column=7, rowspan=4)
 
         self.rec_txt = tk.StringVar(self.ctr_fram, f"Record {self.i}")
         self.record_label = tk.Label(self.ctr_fram, textvariable=self.rec_txt,
                                      relief=tk.SUNKEN)
-        self.record_label.grid(row=0, column=0, columnspan=2)
+        self.record_label.grid(row=row, column=0, columnspan=2)
 
         self.button_bwd = tk.Button(self.ctr_fram, text="Bwd",
                                     command=lambda: self.step(False),
                                     width=w, height=h)
-        self.button_bwd.grid(row=1, column=0)
+        self.button_bwd.grid(row=row + 1, column=0)
         self.button_fwd = tk.Button(self.ctr_fram, text="Fwd",
                                     command=lambda: self.step(True),
                                     width=w, height=h)
-        self.button_fwd.grid(row=1, column=1)
+        self.button_fwd.grid(row=row + 1, column=1)
 
         self.btn_rwd = tk.Button(self.ctr_fram, text="Rewind",
                                  command=lambda: self.thread_run(False),
                                  width=w, height=h)
-        self.btn_rwd.grid(row=2, column=0)
+        self.btn_rwd.grid(row=row + 2, column=0)
 
         self.btn_play = tk.Button(self.ctr_fram, text="Play",
                                   command=lambda: self.thread_run(True),
                                   width=w, height=h)
-        self.btn_play.grid(row=2, column=1)
+        self.btn_play.grid(row=row + 2, column=1)
 
         self.btn_stop = tk.Button(self.ctr_fram, text="Stop",
                                   command=self.thread_stop,
                                   width=w, height=h)
-        self.btn_stop.grid(row=3, column=0, columnspan=2)
+        self.btn_stop.grid(row=row + 3, column=0, columnspan=2)
 
+        # slider
+        row += 4
         self.slider = ttk.Scale(self.window, from_=0, to=self.len - 1,
                                 orient=tk.HORIZONTAL, command=self.slide,)
-        self.slider.grid(row=4, column=0, columnspan=8, sticky='NSEW', padx=10)
+        self.slider.grid(row=row, column=0, columnspan=8, sticky='NSEW',
+                         padx=10)
 
         # quit button
+        row += 1
         self.but_exit = tk.Button(self.window, text="Quit",
                                   command=self.quit,
                                   fg='tomato', borderwidth=0)
-        self.but_exit.grid(row=5, column=7, columnspan=2, sticky=tk.E)
+        self.but_exit.grid(row=row, column=7, columnspan=2, sticky=tk.E)
 
     def step(self, fwd=True):
         self.i += 1 if fwd else -1
@@ -216,6 +246,21 @@ class TubUI:
     def slide(self, val):
         self.i = int(math.floor(float(val)))
         self.update(False)
+
+    def browse_car(self):
+        self.car_dir = filedialog.askdirectory(
+            initialdir=os.path.expanduser('~'),
+            title="Select the car dir")
+        self.car_dir_label.configure(text=self.car_dir)
+        self.config = load_config(os.path.join(self.car_dir, 'config.py'))
+
+    def browse_tub(self):
+        start_dir = self.car_dir if self.car_dir else os.path.expanduser('~')
+        self.base_path = filedialog.askdirectory(initialdir=start_dir,
+                                                 title="Select the tub dir")
+        self.tub_dir_label.config(text=self.base_path)
+        self.update_tub()
+        self.update()
 
     def quit(self):
         self.run = False
