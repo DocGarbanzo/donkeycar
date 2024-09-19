2505bfcfd72d017df753c3248cdc038ecbbef876
DocGarbanzo <dirk.prange@web.de>
2024-09-19 20:33:32 +0100

    Add new gpiozero LED part

--- donkeycar/parts/led_status.py
+++ donkeycar/parts/led_status.py
@@ -225,7 +225,7 @@ class ColorRGB:
     GREEN = (0, 1, 0)
     BLUE = (0, 0, 1)
     YELLOW = (1, 1, 0)
-    ORANGE = (1, 0.5, 0)
+    ORANGE = (1, 0.25, 0.25)
     PURPLE = (0.75, 0, 1)
     WHITE = (1, 1, 1)
     OFF = (0, 0, 0)
