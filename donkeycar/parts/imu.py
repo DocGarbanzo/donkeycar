ab571e012e41261ad308d0b985bdf8f81e568b1c
DocGarbanzo <dirk.prange@web.de>
2024-09-22 12:43:33 +0100

    Add estimated path to IMU part

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -149,7 +149,7 @@ class Mpu6050Ada:
         new_time = time.time()
         delta_t = new_time - self.time
         self.angle += self.gyro[2] * delta_t
-        delta_v_x = self.accel[0] * delta_t * math.cos(self.angle)
+        delta_v_x = self.accel[1] * delta_t * math.cos(self.angle)
         delta_v_y = -self.accel[1] * delta_t * math.sin(self.angle)
         self.speed[0] += delta_v_x
         self.speed[1] += delta_v_y
