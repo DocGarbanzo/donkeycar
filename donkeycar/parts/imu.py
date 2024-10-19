57c3d939681a3de417822125ecee91dc6cd51230
DocGarbanzo <dirk.prange@web.de>
2024-10-19 21:24:50 +0100

    Only print lin accel if valid

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -144,14 +144,14 @@ class Mpu6050Ada:
 
     def calibrate(self):
         logger.info('Calibrating Mpu6050 ...')
-        num_loops = 200
+        num_loops = 400
         gyro = np.zeros(3)
         accel = np.zeros(3)
         accel_norm = 0
         # run w/o doing anything:
-        for _ in range(num_loops//2):
-            tmp = self.mpu.gyro
-            tmp = self.mpu.acceleration
+        # for _ in range(num_loops//2):
+        #     tmp = self.mpu.gyro
+        #     tmp = self.mpu.acceleration
         tic = time.time()
         for _ in range(num_loops):
             gyro += self.mpu.gyro
@@ -161,7 +161,7 @@ class Mpu6050Ada:
             if toc - tic < 1 / self.sample_rate:
                 time.sleep(1 / self.sample_rate - (toc - tic))
             tic = time.time()
-        self.gyro_zero = np.zeros(3) #gyro / num_loops
+        self.gyro_zero = gyro / num_loops
         self.accel_zero = accel / num_loops
         self.accel_norm = accel_norm / num_loops
         logger.info(f'Initial acceleration: {self.accel_zero}, '
