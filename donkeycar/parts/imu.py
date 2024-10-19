bb7cf5050f283b61000dd57971b541b7b4f20ad5
DocGarbanzo <dirk.prange@web.de>
2024-10-19 21:31:33 +0100

    Only print lin accel if valid

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -144,7 +144,7 @@ class Mpu6050Ada:
 
     def calibrate(self):
         logger.info('Calibrating Mpu6050 ...')
-        num_loops = 800
+        num_loops = 1600
         gyro = np.zeros(3)
         accel = np.zeros(3)
         accel_norm = 0
