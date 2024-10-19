4c193ff6892f117d70e5739cd7315cb958973e43
DocGarbanzo <dirk.prange@web.de>
2024-10-19 20:16:27 +0100

    Only print lin accel if valid

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -213,9 +213,9 @@ class Mpu6050Ada:
             # self.lin_accel = np.dot(self.matrix, accel_phys)
             # # remove gravity from world coordinate z-axis
             # self.lin_accel[2] -= self.accel_zero[2]
-            self.lin_accel = 9.81 * self.ahrs.earth_acceleration
             accel_ignore = self.ahrs.internal_states.accelerometer_ignored
             if not accel_ignore:
+                self.lin_accel = 9.81 * self.ahrs.earth_acceleration
                 delta_v = self.lin_accel * dt
                 self.speed += delta_v
             self.pos += (self.speed - self.speed_drift) * dt
