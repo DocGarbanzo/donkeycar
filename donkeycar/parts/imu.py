d67de3b2a0ef23068356de3869f875f7b2a584b0
DocGarbanzo <dirk.prange@web.de>
2025-10-24 23:10:59 +0100

    refactor(bno055): comment out unused position correction logic in BNO055Ada class

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -324,8 +324,8 @@ class BNO055Ada:
             self.pos[2] = 0.0       # z position always zero (2D plane)
 
             # correction term
-            step_distance = self.odometer_speed * dt
-            self.pos[0] -= step_distance * 0.0152
+            # step_distance = self.odometer_speed * dt
+            # self.pos[0] -= step_distance * 0.0152
 
             # Store path with 2D coordinates and odometer speed
             self.path.append(
